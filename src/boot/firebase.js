ddbca7eade96da3ebba3bbc4281083d75920d0f7
Sriram GOPALAN <sriram.gopalan@valeo.com>
2021-05-08 15:14:46 +0530

    Firebase settings added under boot section

---
+++ src/boot/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/database";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBjsMIxu3qUW71kVdhiO4m1Q5YAel0P1sI",
+  authDomain: "testdesk-baa4e.firebaseapp.com",
+  projectId: "testdesk-baa4e",
+  storageBucket: "testdesk-baa4e.appspot.com",
+  messagingSenderId: "697140522147",
+  appId: "1:697140522147:web:50b00945ff62326eca6316"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAuth = firebaseApp.auth();
+const firebaseFireStore = firebaseApp.firestore();
+const firebaseRealTimeDB = firebaseApp.database();
+
+export { firebaseAuth, firebaseRealTimeDB };
